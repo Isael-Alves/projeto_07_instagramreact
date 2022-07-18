2a6565242420d8d171a8aef13b04be298ec8950c
= <isael.alves@tucurui.ufpa.br>
2022-07-18 14:37:49 -0300

    Posts pronto

--- src/components/Body/PartPosts.js
+++ src/components/Body/PartPosts.js
@@ -3,12 +3,12 @@ import Post from './Post'
 export default function PartPosts() {
     const ArrayPosts = [
         {
-            SupImage: "img/meowed 1.svg",
-            name: "meowed",
-            ImagePost: "img/gato-telefone 1.png",
-            InfImage: "img/respondeai 2.svg",
-            curtida: "respondeai",
-            QtdCurtidas: "outras 101.523 pessoas"
+            SupImage: "img/barked 1.svg",
+            name: "barked",
+            ImagePost: "img/dog 1.svg",
+            InfImage: "img/adorableanimals 2.svg",
+            curtida: "adorable_animals",
+            QtdCurtidas: "outras 99.159 pessoas"
         },
         {
             SupImage: "img/meowed 1.svg",
@@ -27,6 +27,7 @@ export default function PartPosts() {
             QtdCurtidas: "outras 101.523 pessoas"
         }
     ]
+    
     return (
         <section className="PartPosts">
             <ul>
--- src/components/Body/Post.js
+++ src/components/Body/Post.js
@@ -1,12 +1,18 @@
+import { IoIosHeartEmpty } from 'react-icons/io';
+import { FiSend } from 'react-icons/fi';
+import { IoChatbubbleOutline } from 'react-icons/io5';
+import { VscBookmark } from 'react-icons/vsc';
+import { FaEllipsisH } from 'react-icons/fa';
+
 export default function Post(props) {
     return (
         <li className="Posts" key={props.i}>
             <div className="Part-Superior-Posts">
                 <div>
-                    <img src ={props.SupImage} alt="" />
+                    <img src={props.SupImage} alt="" />
                     <h3>{props.name}</h3>
                 </div>
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
+                <FaEllipsisH />
             </div>
 
             <div className="Img-Post"><img src={props.ImagePost} alt="" /></div>
@@ -14,11 +20,11 @@ export default function Post(props) {
             <div className="Part-Inferior-Post">
                 <section>
                     <div>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
+                        <IoIosHeartEmpty />
+                        <IoChatbubbleOutline />
+                        <FiSend />
                     </div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
+                    <VscBookmark />
                 </section>
                 <span className="textInferior">
                     <img src={props.InfImage} alt="" />
